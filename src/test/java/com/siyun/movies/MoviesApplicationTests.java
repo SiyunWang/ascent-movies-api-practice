723f0af593fd30f0f5213eb7b11006be6dbb1193
Siyun Wang <wangsiyun0518@gmail.com>
2021-05-16 20:40:19 -0700

    add mysql database

--- src/test/java/com/siyun/movies/MoviesApplicationTests.java
+++ src/test/java/com/siyun/movies/MoviesApplicationTests.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.test.context.TestPropertySource;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +16,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(locations = "classpath:application-test.properties")
 class MoviesApplicationTests {
 	@Autowired
 	MoviesRepository moviesRepository;
